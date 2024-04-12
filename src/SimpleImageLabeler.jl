13c156c0e5d882e0ab8e2e078caf5d53f663f5d5
Eric S. Tellez <donsadit@gmail.com>
2024-04-11 18:52:12 -0600

    versions

--- src/SimpleImageLabeler.jl
+++ src/SimpleImageLabeler.jl
@@ -1,19 +1,36 @@
 module SimpleImageLabeler
 
-using CSV, DataFrames, JSON,ImageView, Images, FileIO, Random, Glob
+using CSV, DataFrames, JSON, ImageView, Images, FileIO, Random, Glob, Comonicon
 
-function main(filename::String; images_path::String, labels::String="0,1", default="0")
+"""
+A simple tool for image labeling
+
+# Args
+
+- `filename`: output file in csv format
+
+# Options
+- `-i, --images-path=<directory>`: directory where images are located
+- `-l, --labels=<labels>`: comma separated labels
+- `-d, --default=<def-label>`: default label (captured with empty string and enter)
+"""
+@main function main(filename::String; images_path::String, labels::String="0,1", default=nothing)
+    valid_labels = split(labels, ',')
+    default = default === nothing ? first(valid_labels) : default
+
     if isfile(filename)
+        @info "loading data from $filename"
         df = CSV.read(filename, DataFrame)
     else
+        @info "reading $images_path files"
         files = glob(joinpath(images_path, "*"))
-        labels = [missing for _ in files]
+        labels = Any[missing for _ in files]
         df = DataFrame(; files, labels)
     end
 
     n = length(df.labels)
     L = shuffle!(findall(ismissing.(df.labels)))
-    @info "loading a labeled table with $(length(L)) labels of $n"
+    @info "using a labeled table with $(length(L)) labels of $n"
 
     while length(L) != 0
         exampleID = pop!(L)
